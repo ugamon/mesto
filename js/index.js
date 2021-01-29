b2f3fa46626acd0515906f62dbd9ce8cb4ebd492
Yuriy Bulich <jurybulich22@gmail.com>
2021-01-29 18:12:26 +0300

    финальная версия на проверку

--- js/index.js
+++ js/index.js
@@ -3,11 +3,8 @@ const popup = root.querySelector('.popup');
 const editButton = root.querySelector('.button_type_edit');
 const closeButton = root.querySelector('.popup__close-button');
 const formElement = popup.querySelector('.popup__form');
-let nameInput = formElement.querySelector('input[name=name]');
-let jobInput = formElement.querySelector('input[name=profession]')
-const saveButton = popup.querySelector('.button_type_save');
-let jobField = root.querySelector('.profile__profession');
-let nameField = root.querySelector('.profile__name');
+
+
 
 const togglePopup = (e, parent=root, className="popup_opened") => {
     const popup = parent.querySelector('.popup');
@@ -17,7 +14,14 @@ const togglePopup = (e, parent=root, className="popup_opened") => {
 }
 
 const submitHandler = (e) => {
+    let nameInput = formElement.querySelector('input[name=name]');
+    let jobInput = formElement.querySelector('input[name=profession]')
+    
+    let jobField = root.querySelector('.profile__profession');
+    let nameField = root.querySelector('.profile__name');
+
     e.preventDefault();
+    
     nameField.textContent = nameInput.value;
     jobField.textContent = jobInput.value;
 
