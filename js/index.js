7cd61718d10a56fc7f9ee474897bcda064952ae2
Yuriy Bulich <jurybulich22@gmail.com>
2021-02-07 11:55:18 +0300

    (feature) теперь карточки подгружаются через js

--- js/index.js
+++ js/index.js
@@ -9,8 +9,51 @@ let jobInput = formElement.querySelector('input[name=profession]')
 let jobField = root.querySelector('.profile__profession');
 let nameField = root.querySelector('.profile__name');
 
+const initialCards = [
+    {
+      name: 'Архыз',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg',
+      alt: 'Архыз'
+    },
+    {
+      name: 'Челябинская область',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg',
+      alt: 'Челябинская область'
+    },
+    {
+      name: 'Иваново',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg',
+      alt: 'Иваново'
+    },
+    {
+      name: 'Камчатка',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg',
+      alt: 'Камчатка'
+    },
+    {
+      name: 'Холмогорский район',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg',
+      alt: 'Холмогорский район'
+    },
+    {
+      name: 'Байкал',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg',
+      alt: 'Байкал'
+    }
+  ]; 
 
-/* спасибо за подсказку, ступил =) теперь понятно что нужно было сделать. */
+
+const placeTemplate = document.querySelector('#place-template').content;
+const placeContainer = document.querySelector('.place-container');
+
+
+initialCards.map((item) => {
+    const placeCard = placeTemplate.querySelector('.place').cloneNode(true);
+    placeCard.querySelector('.place__image').src = item.link;
+    placeCard.querySelector('.place__image').alt = item.alt;
+    placeCard.querySelector('.place__title').textContent = item.name;
+    placeContainer.appendChild(placeCard)
+})
 
 
 const openPopup = (e) => {
